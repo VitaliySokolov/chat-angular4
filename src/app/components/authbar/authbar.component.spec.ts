e72197c5e49b69708ebc3aceff6e484b4b1c9966
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2017-08-25 17:17:38 +0300

    Update authbar component unit tests

--- src/app/components/authbar/authbar.component.spec.ts
+++ src/app/components/authbar/authbar.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthbarComponent } from './authbar.component';
+import {AuthbarComponent} from './authbar.component';
+import {By} from '@angular/platform-browser';
 
 describe('AuthbarComponent', () => {
   let component: AuthbarComponent;
   let fixture: ComponentFixture<AuthbarComponent>;
+  const
+    user1 = {name: 'John Doe'},
+    user2 = {name: 'Micky Mouse'};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,37 @@ describe('AuthbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AuthbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display user name', () => {
+    const expectedName = user1.name;
+    component.user = user1;
+    fixture.detectChanges();
+    const userName = fixture.debugElement.query(By.css('.user-name')).nativeElement;
+    expect(userName.textContent.trim()).toBe(expectedName);
+    expect(userName.textContent.trim()).not.toBe('');
+  });
+
+  it('should indicate online', () => {
+    const expectedClass = 'green';
+    component.user = user1;
+    component.online = true;
+    fixture.detectChanges();
+    const avatar = fixture.debugElement.query(By.css('.avatar')).nativeElement;
+    expect(avatar.classList).toContain(expectedClass);
+  });
+
+  it('should indicate offline', () => {
+    const expectedClass = 'green';
+    component.user = user1;
+    component.online = false;
+    fixture.detectChanges();
+    const avatar = fixture.debugElement.query(By.css('.avatar')).nativeElement;
+    expect(avatar.classList).not.toContain(expectedClass);
+
   });
 });
