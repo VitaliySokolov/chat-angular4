4f49eaad05e907fb3e3b2689b3bca31aebcfcc11
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2017-09-02 10:44:14 +0300

    Test & fix async2array pipe

--- src/app/pipes/async2array.pipe.spec.ts
+++ src/app/pipes/async2array.pipe.spec.ts
@@ -5,4 +5,34 @@ describe('Async2arrayPipe', () => {
     const pipe = new Async2arrayPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should convert an empty object to null ', () => {
+    const pipe = new Async2arrayPipe();
+    expect(pipe.transform({})).toBeNull();
+  });
+
+  it('should convert to null when object w/o `items` property', () => {
+    const pipe = new Async2arrayPipe();
+    expect(pipe.transform({ foo: 'bar' })).toBeNull();
+  });
+
+  it('should convert to null when `items` property is null', () => {
+    const pipe = new Async2arrayPipe();
+    expect(pipe.transform({ items: null })).toBeNull();
+  });
+
+  it('should convert to null if inputed `items` is not object', () => {
+    const pipe = new Async2arrayPipe();
+    expect(pipe.transform({ items: 'bar' })).toBeNull();
+  });
+
+  it('should convert the values of `items` to array', () => {
+    const pipe = new Async2arrayPipe();
+    expect(pipe.transform({
+      items: {
+        id1: 'foo',
+        id2: 'bar'
+      }
+    })).toEqual(['foo', 'bar']);
+  });
 });
--- src/app/pipes/async2array.pipe.ts
+++ src/app/pipes/async2array.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+// todo: rename to unnormalizingState or itemsValueArray
 @Pipe({
   name: 'async2array',
   pure: false
@@ -7,10 +7,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Async2arrayPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if (value && 'items' in value) {
+    if (value && 'items' in value
+      && value.items && typeof value.items === 'object') {
       return Object.keys(value.items).map(key => value.items[key]);
     } else {
-      return;
+      return null;
     }
   }
 
